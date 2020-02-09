6c832a4d786cb736eb8bd252edd7fb3bcf772035
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-02-08 16:29:18 -0800

    Add files via upload

---
+++ springProjects/authentication/src/main/java/com/Irene/authentication/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Irene.authentication.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Irene.authentication.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+}
+
